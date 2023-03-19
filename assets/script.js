0ed9d3cffca824a1b98599b1a51884bc81f08e01
cassiegidd <124100826+cassiegidd@users.noreply.github.com>
2023-03-18 19:59:41 -0600

    created function to test for value of checked option and reduce time for false value

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,14 @@ var startBtn = document.querySelector("#start-button");
 var timer = document.querySelector("#count-down");
 var timeLeft = 60; //do i need this in the global and do i need to pass this value in the global variable?
 var fieldset1 = document.querySelector("#fieldset1");
+var fieldset2 = document.querySelector("#fieldset2");
 var submitBtn1 = document.querySelector("#submit-button-one");
-var questions = document.querySelector(".questions");
-var labelOne = document.querySelector("#option-one");
-var labelTwo = document.querySelector("#option-two");
-var labelThree = document.querySelector("#option-three");
-var labelFour = document.querySelector("#option-four");
-const questionOptions = {
-    question1: "An array...",
-    question2: "Which data type returns a true/false value?"
-}
-
+var answer1 = document.querySelector("input[id=q1a4]");//creating a variable witht the value of the CORRECT answer for question one
+var answer2 = document.querySelector("input[id=q2a2]");//creating a variable with the value of the CORRECT answer for question two 
+var submitBtn2 = document.querySelector("#submit-button-two");
+// at the click of the START button, this function will create and start a timer countdown and make visible the first question form
 startBtn.addEventListener("click", function () {
+    startBtn.style.visibility = "hidden";
     fieldset1.removeAttribute("hidden");
     var timerInterval = setInterval(function() {
         timeLeft--;
@@ -22,10 +18,25 @@ startBtn.addEventListener("click", function () {
             clearInterval(timerInterval);
         }    
     }, 1000);
-    startBtn.style.visibility = "hidden";
 })
+
+// at the click of the SUBMIT button, this function will check if the checked option has a value of true/false (no value=false) and will subtract time from timer if value returns false
 submitBtn1.addEventListener("click", function () {
-    console.log("hello");
+    fieldset1.hidden = true;
+    fieldset2.removeAttribute("hidden");
+    if (answer1.checked===false) {
+        console.log(answer1.value);
+        timeLeft--;
+        timeLeft--;
+    }
+})
+submitBtn2.addEventListener("click", function () {
+    fieldset2.hidden = true;
+    if (answer2.checked===false) {
+        console.log(answer2.value);
+        timeLeft--;
+        timeLeft--;  
+    }
 })
 
 
