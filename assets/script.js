2ee086c3a3a18a715d267b3d2fbd8a938e4520cb
cassiegidd <124100826+cassiegidd@users.noreply.github.com>
2023-03-20 16:38:45 -0600

    added more styling and added elements to enter score

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var startBtn = document.querySelector("#start-button");
 var timer = document.querySelector("#count-down");
 var timeLeft = 60; //do i need this in the global and do i need to pass this value in the global variable?
+var quizEnd = false;
 var fieldset1 = document.querySelector("#fieldset1");
 var fieldset2 = document.querySelector("#fieldset2");
 var fieldset3 = document.querySelector("#fieldset3");
@@ -15,6 +16,17 @@ var submitBtn4 = document.querySelector("#submit-button-four");
 var submitBtn5 = document.querySelector("#submit-button-five");
 var submitBtn6 = document.querySelector("#submit-button-six");
 var submitBtn7 = document.querySelector("#submit-button-seven");
+var wrong = document.createElement("p");
+var correct = document.createElement("p");
+var gameOver = document.querySelector("#game-over");
+var quiz = document.querySelector("#quiz-content");
+
+
+var yourScore = document.querySelector("#your-score");
+
+// var scores = JSON.parse(localStorage.getItem("scores")) ?? [];
+
+
 // var answer1 = document.querySelector("input[name=answer1]");
 // var answer2 = document.querySelector("input[name=answer2]");
 // var answer3 = document.querySelector("input[name=answer3]");
@@ -26,12 +38,16 @@ var submitBtn7 = document.querySelector("#submit-button-seven");
 // at the click of the START button, this function will create and start a timer countdown and make visible the first question form
 startBtn.addEventListener("click", function () {
     startBtn.style.visibility = "hidden";
+    quiz.removeAttribute("hidden");
     fieldset1.removeAttribute("hidden");
     var timerInterval = setInterval(function() {
         timeLeft--;
         timer.textContent = timeLeft;
-        if(timeLeft ===0) {
+        if(timeLeft === 0 || quizEnd) {
             clearInterval(timerInterval);
+            gameOver.removeAttribute("hidden");
+            quiz.hidden = true;
+            yourScore.removeAttribute("hidden");
         }    
     }, 1000);
 })
@@ -42,17 +58,29 @@ submitBtn1.addEventListener("click", function () {
     fieldset2.removeAttribute("hidden");
     if (document.getElementById("q1a4").checked===false) {  
         console.log("test");
-        timeLeft--;
-        timeLeft--;
-    }   
+        timeLeft-=3;
+        wrong.innerHTML="WRONG!";
+        wrong.classList.add("wrong-class");
+        fieldset2.appendChild(wrong);
+    } else {
+        correct.innerHTML="CORRECT!";
+        correct.classList.add("correct-class");
+        fieldset2.appendChild(correct);
+    }  
 })
 submitBtn2.addEventListener("click", function () {
     fieldset2.hidden = true;
     fieldset3.removeAttribute("hidden");
     if (document.getElementById("q2a2").checked===false) {
         console.log("test2");
-        timeLeft--;
-        timeLeft--;  
+        timeLeft-=3;
+        wrong.innerHTML="WRONG!";
+        wrong.classList.add("wrong-class");
+        fieldset3.appendChild(wrong); 
+    } else {
+        correct.innerHTML="CORRECT!";
+        correct.classList.add("correct-class");
+        fieldset3.appendChild(correct);
     }
 })
 submitBtn3.addEventListener("click", function () {
@@ -60,8 +88,14 @@ submitBtn3.addEventListener("click", function () {
     fieldset4.removeAttribute("hidden");
     if (document.getElementById("q3a1").checked===false) {
         console.log("test3");
-        timeLeft--;
-        timeLeft--;  
+        timeLeft-=3;
+        wrong.innerHTML="WRONG!";
+        wrong.classList.add("wrong-class");
+        fieldset4.appendChild(wrong); 
+    } else {
+        correct.innerHTML="CORRECT!";
+        correct.classList.add("correct-class");
+        fieldset4.appendChild(correct); 
     }
 })
 submitBtn4.addEventListener("click", function () {
@@ -69,8 +103,14 @@ submitBtn4.addEventListener("click", function () {
     fieldset5.removeAttribute("hidden");
     if (document.getElementById("q4a3").checked===false) {
         console.log("test4");
-        timeLeft--;
-        timeLeft--;  
+        timeLeft-=3;
+        wrong.innerHTML="WRONG!";
+        wrong.classList.add("wrong-class");
+        fieldset5.appendChild(wrong); 
+    } else {
+        correct.innerHTML="CORRECT!";
+        correct.classList.add("correct-class");
+        fieldset5.appendChild(correct);  
     }
 })
 submitBtn5.addEventListener("click", function () {
@@ -78,8 +118,14 @@ submitBtn5.addEventListener("click", function () {
     fieldset6.removeAttribute("hidden");
     if (document.getElementById("q5a3").checked===false) {
         console.log("test5");
-        timeLeft--;
-        timeLeft--;  
+        timeLeft-=3;
+        wrong.innerHTML="WRONG!";
+        wrong.classList.add("wrong-class");
+        fieldset6.appendChild(wrong); 
+    } else {
+        correct.innerHTML="CORRECT!";
+        correct.classList.add("correct-class");
+        fieldset6.appendChild(correct);  
     }
 })
 submitBtn6.addEventListener("click", function () {
@@ -87,16 +133,38 @@ submitBtn6.addEventListener("click", function () {
     fieldset7.removeAttribute("hidden");
     if (document.getElementById("q6a1").checked===false) {
         console.log("test6");
-        timeLeft--;
-        timeLeft--;  
+        timeLeft-=3;
+        wrong.innerHTML="WRONG!";
+        wrong.classList.add("wrong-class");
+        fieldset7.appendChild(wrong); 
+    } else {
+        correct.innerHTML="CORRECT!";
+        correct.classList.add("correct-class");
+        fieldset7.appendChild(correct);
     }
 })
 submitBtn7.addEventListener("click", function () {
     if (document.getElementById("q7a4").checked===false) {
         console.log("test7");
-        timeLeft--;
-        timeLeft--;
+        timeLeft-=3;
+        wrong.innerHTML="WRONG!";
+        wrong.classList.add("wrong-class");
+        quiz.appendChild(wrong); 
+    } else {
+        correct.innerHTML="CORRECT!";
+        correct.classList.add("correct-class");
+        quiz.appendChild(correct);
     }
+    quizEnd = true;
+    
+
+    
+
+    // var player = prompt("Please provide you name?");
+    // var playerExist = scores.find(obj => obj.player === player);
+    // if(!playerExist){
+    //     scores = scores.push({ player: player, score: (timeLeft - 1)});
+    //     localStorage.setItem("scores", scores);   
 })
 
 
